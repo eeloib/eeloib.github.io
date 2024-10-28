177807c5968771d781f723dfec4bae6e1dcc3ea2
Elias Eloi Braga <eeloib@gmail.com>
2024-10-28 17:20:11 -0300

    Update script.js

--- VetorExperience/script.js
+++ VetorExperience/script.js
@@ -5,24 +5,27 @@ const handleSubmit = (event)=>{
     event.preventDefault();
     fetch('https://script.google.com/macros/s/AKfycbxWRBwMEBilng8eIjFzDdykiIqGsseyQcilo4NwhKTM7EiT82A3PUCEfMv5scYVuEAE/exec',{
         method: 'POST',
+        mode: 'cors',
+        credentials: 'include', // include, *same-origin, omit
+        redirect: 'follow',
         headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Content-Type': 'text/plain;charset=utf-8',
         },
         body: JSON.stringify({
-            "Nome": nome.value,
-            "Telefone": telefone.value,
-            "Email": email.value,
-            "Placa":placa.value,
-            'CPF':cpf.value,
-            'DataNascimento':dataNascimento.value,
-            'Cidade':cidade.value,
-            'Estado':estado.value,
-            'LinkZap':LinkZap.value,
-            'LinkQrCode':LinkQrCode.value
-        })
-    }).then(response => {
-        console.log("success:", response);
-    });
+                "Nome": nome.value,
+                "Telefone": telefone.value,
+                "Email": email.value,
+                "Placa":placa.value,
+                'CPF':cpf.value,
+                'DataNascimento':dataNascimento.value,
+                'Cidade':cidade.value,
+                'Estado':estado.value,
+                'LinkZap':LinkZap.value,
+                'LinkQrCode':LinkQrCode.value
+            })
+        }).then(response => {
+            console.log("success:", response);
+        });
 }
 
 document.querySelector('form').addEventListener('submit',handleSubmit);
