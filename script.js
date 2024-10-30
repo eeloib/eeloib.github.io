0bef81343b14d01c17e9af10cf29b41559114e82
Elias Eloi Braga <eeloib@gmail.com>
2024-10-30 17:42:36 -0300

    Changes

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 
-    import * as jwt_decode from 'jwt-decode';
-      
-      function handleCredentialResponse(response) {
-        const data = jwt_decode(response.credential);
-        console.log(data)  
-      }
-      window.onload = function () {
-        google.accounts.id.initialize({
-          client_id: "512160770236-16n573eolsvjjmoflkl5hlaku9ha22pe.apps.googleusercontent.com",
-          callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-          document.getElementById("buttonDiv"),
-          { 
-            theme: "filled_black", 
-            size: "large",
-            type: "standard",
-            shape: "pill",
-            text: "continue_with",
-            logo_alignment: "left",
-            width: "400" 
-          }  // customization attributes
-        );
-        google.accounts.id.prompt(); // also display the One Tap dialog
-      }
-
 const handleSubmit = (event)=>{
     event.preventDefault();
     fetch('https://script.google.com/macros/s/AKfycbxWRBwMEBilng8eIjFzDdykiIqGsseyQcilo4NwhKTM7EiT82A3PUCEfMv5scYVuEAE/exec',{
